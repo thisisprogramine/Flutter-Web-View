41545486fe228fbbbe2527c3516c2e66183d2099
Siddhant Mahamuni <siddhantmahamuni@gmail.com>
2023-05-17 16:57:30 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter WebView',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isLoading = true;
+
+  late WebViewController webView;
+
+  Future<bool> _onBack() async {
+    var value = await webView.canGoBack();
+
+    if (value) {
+      await webView.goBack();
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () => _onBack(),
+      child: Scaffold(
+        body: SafeArea(
+          child: Stack(
+            children: [
+              WebView(
+                initialUrl: 'https://agrogenix.farm/',
+                javascriptMode: JavascriptMode.unrestricted,
+                onPageStarted: (url) {
+                  setState(() {
+                    isLoading = true;
+                  });
+                },
+                onPageFinished: (status) {
+                  setState(() {
+                    isLoading = false;
+                  });
+                },
+                onWebViewCreated: (WebViewController controller) {
+                  webView = controller;
+                },
+              ),
+              isLoading
+                  ? Center(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 50.0, vertical: 20.0),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25.0)),
+                    child: CircularProgressIndicator(),
+                  ))
+                  : Stack(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
